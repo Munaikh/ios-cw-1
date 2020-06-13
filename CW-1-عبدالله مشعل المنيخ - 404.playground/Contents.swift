43a7ff0a31807f953185693d09290784c28712f6
عبدالله مشعل المنيخ -404 <66527073+Munaikh@users.noreply.github.com>
2020-06-13 18:52:51 +0300

    done

---
+++ CW-1-عبدالله مشعل المنيخ - 404.playground/Contents.swift
@@ -0,0 +1,13 @@
+
+
+let myname = "عبدالله المنيخ"
+
+var age = 15
+
+var height = 180
+
+var hobby1 = "السباحة"
+
+var hobby2 = "التصميم"
+
+print("السلام عليكم، اسمي \(myname) انا احب \(hobby1) و \(hobby2) عمري \(age) وطولي \(height)" )
