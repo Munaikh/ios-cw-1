ff141e351a8596e07a703169235412250aaeb612
عبدالله مشعل المنيخ -404 <66527073+Munaikh@users.noreply.github.com>
2020-06-13 19:04:56 +0300

    اضافات جديدة
    المقارنة بيني وبين صديقي

--- CW-1-عبدالله مشعل المنيخ - 404.playground/Contents.swift
+++ CW-1-عبدالله مشعل المنيخ - 404.playground/Contents.swift
@@ -1,4 +1,4 @@
-
+// معلوماتي
 
 let myname = "عبدالله المنيخ"
 
@@ -11,3 +11,22 @@ var hobby1 = "السباحة"
 var hobby2 = "التصميم"
 
 print("السلام عليكم، اسمي \(myname) انا احب \(hobby1) و \(hobby2) عمري \(age) وطولي \(height)" )
+
+// معلومات صديقي
+
+let firendName = "محمد"
+
+var friendAge = 14
+
+var friendHeight = 170
+
+var friendHobby1 = "الرسم"
+
+print("السلام عليكم، اسمي \(firendName) انا احب \(friendHobby1)  عمري \(friendAge) وطولي \(friendHeight)" )
+
+//المقارنة
+
+var age_difference = age > friendAge
+
+var height_difference = height < friendHeight
+
